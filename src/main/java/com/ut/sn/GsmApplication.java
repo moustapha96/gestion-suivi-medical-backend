9af18585d1282b6fe44a441126552d8b1e4eb9e3
Al hussein Khouma/UIDT <68778997+moustapha96@users.noreply.github.com>
2022-01-12 10:39:05 +0000

    add swagger

--- src/main/java/com/ut/sn/GsmApplication.java
+++ src/main/java/com/ut/sn/GsmApplication.java
@@ -2,9 +2,13 @@ package com.ut.sn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+@Configuration
+@EnableSwagger2WebMvc
 @SpringBootApplication
 @EnableSwagger2
 public class GsmApplication {
